ce254f27ab66e49e3394c53680d18329b37c35a7
Austin Wolfe <wolfe_austin@yahoo.com>
2018-05-31 16:02:17 -0400

    Done.

--- src/reducers/manageFriends.js
+++ src/reducers/manageFriends.js
@@ -1,2 +1,10 @@
-export function manageFriends(state, action){
+export function manageFriends(state, action) {
+  switch (action.type) {
+    case "ADD_FRIEND":
+      return { friends: [...state.friends, action.friend] };
+    case "REMOVE_FRIEND":
+      return { friends: state.friends.filter(f => f.id != action.id) };
+    default:
+      return state;
+  }
 }
